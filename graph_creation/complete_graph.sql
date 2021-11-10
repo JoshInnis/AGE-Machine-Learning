cc101557bd9b9d73b4c6b26d4bd45d821369d4cb
Josh Innis <JoshInnis@gmail.com>
2021-11-09 19:29:29 -0800

    Added Complete Graph Function
    Added Function that will create a graph where all vertices
    are conected to all other vertices

---
+++ graph_creation/complete_graph.sql
@@ -0,0 +1,40 @@
+CREATE OR REPLACE FUNCTION age_ml.create_path_part(a int, b int)
+RETURNS VARCHAR
+LANGUAGE plpgsql
+AS $function$
+DECLARE sql VARCHAR;
+BEGIN
+	sql := format('(a_%s)-[:edge]->(a_%s), (a_%s)<-[:edge]-(a_%s)', a, b, a, b);
+	RETURN sql;
+END
+$function$;
+
+CREATE OR REPLACE FUNCTION age_ml.create_complete_graph(graph_name name, n int)
+RETURNS VOID
+LANGUAGE plpgsql
+AS $function$
+DECLARE trans agtype;
+        sql VARCHAR;
+BEGIN
+        load 'age';
+        SET search_path TO ag_catalog;
+
+	PERFORM create_graph(graph_name);
+
+	sql := format('SELECT * FROM cypher(''%s'', $$ CREATE ', graph_name);
+
+	FOR i IN 1..n LOOP
+		FOR j IN i+1 .. n LOOP
+			SELECT concat(sql, age_ml.create_path_part(i, j))
+			INTO sql;
+		END LOOP; 
+	END LOOP;
+
+	SELECT CONCAT(sql, '$$) AS (a agtype);')
+	INTO sql;
+
+	EXECUTE sql;
+
+END
+$function$;
+
